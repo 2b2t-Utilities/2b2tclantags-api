bef76b91d4d6fff908661ae8d9ea13ad1dc6fea3
Tigermouthbear <tigermouthbear@gmail.com>
2020-01-01 23:45:34 -0500

    updated version number

--- src/main/java/me/tigermouthbear/clantags/ClanTags.java
+++ src/main/java/me/tigermouthbear/clantags/ClanTags.java
@@ -20,7 +20,7 @@ public class ClanTags
 {
     public static final String MODID = "2b2tclantags";
     public static final String NAME = "2b2tclantags";
-    public static final String VERSION = "0.1";
+    public static final String VERSION = "1.0";
 
     @EventHandler
     public void init(FMLInitializationEvent event)
