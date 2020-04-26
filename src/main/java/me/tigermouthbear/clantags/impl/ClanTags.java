584e7a760bed93ccb522f2e469f318c3c8ccc260
Tigermouthbear <tigermouthbear@gmail.com>
2020-04-25 20:45:08 -0400

    updated version number

--- src/main/java/me/tigermouthbear/clantags/impl/ClanTags.java
+++ src/main/java/me/tigermouthbear/clantags/impl/ClanTags.java
@@ -20,7 +20,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class ClanTags {
 	public static final String MODID = "2b2tclantags";
 	public static final String NAME = "2b2tclantags";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "2.1";
 
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
